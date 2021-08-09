11f887ed212823c2776c9c41281d090d14fe744f
Trakarn Jamlongpaksakun <takarn@wisdomvast.com>
2021-08-09 13:49:45 +0700

    UIViewController : add present root view controller

---
+++ Sources/JetpackKit/Extension/JPK+Extension+UIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Trakarn Jamlongpaksakun on 9/8/2564 BE.
+//
+
+import Foundation
+import UIKit
+
+public extension UIViewController {
+    
+    /// Present and set nav controller 
+    public present(
+        rootViewController: UIViewController,
+        modalPresentStyle: UIModalPresentationStyle? = nil,
+        animated: Bool = true,
+        completion: @escaping (() -> Void)?) -> Void {
+        
+        let nav = UINavigationController(rootViewController: rootViewController)
+        if let modalStyle = modalPresentStyle {
+            nav.modalPresentationStyle = modalStyle
+        }
+        
+        present(nav, animated: animated, completion: completion)
+        
+    }
+    
+}
